b7f035780c94b91e85519fc24e136d849449d9b9
3Shain <san3shain@outlook.com>
2021-10-29 18:37:53 +0800

    fix(vue-lib): provide standalone .d.ts for IDE interop

---
+++ libs/vue-lib/interop.d.ts
@@ -0,0 +1,8 @@
+import type { Ref } from 'vue';
+
+declare module 'kairo' {
+  // I'm gonna tell a lie and hurt you (vue-language-service) 😭
+  interface Cell<T> extends Ref<T> {
+    readonly value: T;
+  }
+}
